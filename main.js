11a2a5ee92b285f32b9bc1f60e4879ff8746ead1
roman562n <shpakroman562@gmail.com>
2025-12-17 00:39:23 +0300

    A little more styling, working parameters

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 const canvasElemant = document.querySelector('[data-js-canvas]')
-const generateButtonElement = document.querySelector('[data-js-generate-button]')
 const paletteSelectElement = document.querySelector('[data-js-palette-select]')
+const paletteResolutionInputElements = document.querySelectorAll('[data-js-palette-resolution-input]')
+const pixelSizeInputElement = document.querySelector('[data-js-palette-resolution-pixel-size]')
+const generateButtonElement = document.querySelector('[data-js-generate-button]')
 const customizeMenuElement = document.querySelector('[data-js-customize-menu]')
 const colorSelectElement = customizeMenuElement.querySelector('[data-js-color-select-inputs]')
 const colorAddButtonElement = customizeMenuElement.querySelector('[data-js-color-select-add-button]')
 const customizeMenuCancelButtonElement = customizeMenuElement.querySelector('[data-js-customize-menu-cancel-button]')
 
-const ctx = canvasElemant.getContext('2d')
-const imageSize = [16, 16]
-const pixelSize = 20
+const canvasSizeUpdate = () => {
+  canvasElemant.width = imageSize[0] * pixelSize
+  canvasElemant.height = imageSize[1] * pixelSize
+}
 
-const palettes = [
+const ctx = canvasElemant.getContext('2d')
+let imageSize = [16, 16]
+let pixelSize = 20
+canvasSizeUpdate()
+
+const defaultPalettes = [
   { id: "pt1", value: 'random', name: "Случайные цвета" },
   { id: "pt2", value: 'blue', name: "Синяя", colors: ['#003153', '#4285B4', '#3079d8ff', '#002F55', '#0E294B', '#1560BD'] },
   { id: "pt3", value: 'pink', name: "Розовая", colors: ['#fa1e67', '#FF1493', '#D71868', '#FF43A4', '#EF0097', '#FF00CC'] },
   { id: "pt4", value: 'gray', name: "Серая", colors: ['#5a5a5a', '#252525', '#333333', '#575757', '#949494', '#979797'] },
   { id: "ptcu", value: 'custom', name: "+ Создать свою" },
 ]
-
-canvasElemant.width = imageSize[0] * pixelSize
-canvasElemant.height = imageSize[1] * pixelSize
 
 const getRandomNumber = (min = 0, max = 256) => {
   const number = Math.floor(Math.random() * (max + 1 - min)) + min
@@ -38,7 +43,7 @@ const findPalette = (palette) => {
 const generateImage = () => {
   ctx.clearRect(0, 0, canvasElemant.width, canvasElemant.height)
 
-  const findedPalette = palettes.find(findPalette)
+  const findedPalette = defaultPalettes.find(findPalette)
 
   for (let i = 0; i < imageSize[0]; i++) {
     for (let j = 0; j < imageSize[1]; j++) {
@@ -57,7 +62,7 @@ const generateImage = () => {
   }
 }
 
-palettes.map((palette) => {
+defaultPalettes.map((palette) => {
   const newOption = `<option class="palette__option" id="${palette.id}" value="${palette.value}">${palette.name}</option>`
   paletteSelectElement.insertAdjacentHTML("beforeend", newOption)
 })
@@ -68,8 +73,10 @@ generateButtonElement.addEventListener('click', (event) => {
 
   if (selectedPalette === "") {
     console.log('Палитра не выбрана')
+    paletteSelectElement.style.boxShadow = "-3px 3px 0 0 #c00f2cff"
   }
   else {
+    paletteSelectElement.style.boxShadow = "-3px 3px 0 0 #2a0e30"
     generateImage()
   }
 })
@@ -87,6 +94,18 @@ colorAddButtonElement.addEventListener('click', () => {
 
 customizeMenuCancelButtonElement.addEventListener('click', () => {
   customizeMenuElement.classList.remove('is-active')
+})
+
+paletteResolutionInputElements.forEach((element, index) => {
+  element.addEventListener("change", (event) => {
+    imageSize[index] = event.target.value
+    canvasSizeUpdate()
+  })
+})
+
+pixelSizeInputElement.addEventListener('change', (event) => {
+  pixelSize = event.target.value
+  canvasSizeUpdate()
 })
 
 // colorSelectElement.addEventListener('change', (event) => {
