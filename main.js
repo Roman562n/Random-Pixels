e482bf48ab179cf4478b3cc0d898000dd8c675a3
roman562n <shpakroman562@gmail.com>
2025-12-27 00:26:25 +0300

    Added validation of entered data

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const canvasElemant = document.querySelector('[data-js-canvas]')
+const controlsElement = document.querySelector('[data-js-controls]')
 const paletteSelectElement = document.querySelector('[data-js-palette-select]')
 const paletteResolutionInputElements = document.querySelectorAll('[data-js-palette-resolution-input]')
 const pixelSizeInputElement = document.querySelector('[data-js-palette-resolution-pixel-size]')
@@ -7,6 +8,27 @@ const customizeMenuElement = document.querySelector('[data-js-customize-menu]')
 const colorSelectElement = customizeMenuElement.querySelector('[data-js-color-select-inputs]')
 const colorAddButtonElement = customizeMenuElement.querySelector('[data-js-color-select-add-button]')
 const customizeMenuCancelButtonElement = customizeMenuElement.querySelector('[data-js-customize-menu-cancel-button]')
+
+const defaultPalettes = [
+  { id: "pt1", value: 'random', name: "Случайные цвета" },
+  { id: "pt2", value: 'blue', name: "Синяя", colors: ['#003153', '#4285B4', '#3079d8ff', '#002F55', '#0E294B', '#1560BD'] },
+  { id: "pt3", value: 'pink', name: "Розовая", colors: ['#fa1e67', '#FF1493', '#D71868', '#FF43A4', '#EF0097', '#FF00CC'] },
+  { id: "pt4", value: 'gray', name: "Серая", colors: ['#5a5a5a', '#252525', '#333333', '#575757', '#949494', '#979797'] },
+  { id: "ptcu", value: 'custom', name: "+ Создать свою" },
+]
+
+localStorage.setItem('palettes', JSON.stringify([{ id: "pt10", value: 'red', name: "Красная", colors: ['#003153', '#4285B4', '#3079d8ff', '#002F55', '#0E294B', '#1560BD'] }]))
+
+let storedPalettes
+!localStorage.palettes ? storedPalettes = [] : storedPalettes = JSON.parse(localStorage.getItem('palettes'))
+// console.log(storedPalettes)
+
+const palettes = defaultPalettes.concat(storedPalettes)
+
+palettes.map((palette) => {
+  const newOption = `<option class="palette__option" id="${palette.id}" value="${palette.value}">${palette.name}</option>`
+  paletteSelectElement.insertAdjacentHTML("beforeend", newOption)
+})
 
 const canvasSizeUpdate = () => {
   canvasElemant.width = imageSize[0] * pixelSize
@@ -17,14 +39,6 @@ const ctx = canvasElemant.getContext('2d')
 let imageSize = [16, 16]
 let pixelSize = 20
 canvasSizeUpdate()
-
-const defaultPalettes = [
-  { id: "pt1", value: 'random', name: "Случайные цвета" },
-  { id: "pt2", value: 'blue', name: "Синяя", colors: ['#003153', '#4285B4', '#3079d8ff', '#002F55', '#0E294B', '#1560BD'] },
-  { id: "pt3", value: 'pink', name: "Розовая", colors: ['#fa1e67', '#FF1493', '#D71868', '#FF43A4', '#EF0097', '#FF00CC'] },
-  { id: "pt4", value: 'gray', name: "Серая", colors: ['#5a5a5a', '#252525', '#333333', '#575757', '#949494', '#979797'] },
-  { id: "ptcu", value: 'custom', name: "+ Создать свою" },
-]
 
 const getRandomNumber = (min = 0, max = 256) => {
   const number = Math.floor(Math.random() * (max + 1 - min)) + min
@@ -43,7 +57,7 @@ const findPalette = (palette) => {
 const generateImage = () => {
   ctx.clearRect(0, 0, canvasElemant.width, canvasElemant.height)
 
-  const findedPalette = defaultPalettes.find(findPalette)
+  const findedPalette = palettes.find(findPalette)
 
   for (let i = 0; i < imageSize[0]; i++) {
     for (let j = 0; j < imageSize[1]; j++) {
@@ -62,21 +76,55 @@ const generateImage = () => {
   }
 }
 
-defaultPalettes.map((palette) => {
-  const newOption = `<option class="palette__option" id="${palette.id}" value="${palette.value}">${palette.name}</option>`
-  paletteSelectElement.insertAdjacentHTML("beforeend", newOption)
-})
-
-generateButtonElement.addEventListener('click', (event) => {
-  const selectedPalette = paletteSelectElement.value
-  event.preventDefault()
-
-  if (selectedPalette === "") {
-    console.log('Палитра не выбрана')
+const inputVerification = () => {
+  let answer = true
+  if (paletteSelectElement.value === "") {
     paletteSelectElement.style.boxShadow = "-3px 3px 0 0 #c00f2cff"
+    answer = false
   }
   else {
     paletteSelectElement.style.boxShadow = "-3px 3px 0 0 #2a0e30"
+  }
+  paletteResolutionInputElements.forEach((element, index) => {
+    if (element.value <= 0) {
+      element.style.boxShadow = "-3px 3px 0 0 #c00f2cff"
+      answer = false
+    }
+    else {
+      element.style.boxShadow = "-3px 3px 0 0 #2a0e30"
+      imageSize[index] = element.value
+    }
+  })
+  if (pixelSizeInputElement.value <= 0) {
+    pixelSizeInputElement.style.boxShadow = "-3px 3px 0 0 #c00f2cff"
+    answer = false
+  }
+  else {
+    pixelSizeInputElement.style.boxShadow = "-3px 3px 0 0 #2a0e30"
+    pixelSize = pixelSizeInputElement.value
+  }
+
+  canvasSizeUpdate()
+  return answer
+}
+
+controlsElement.addEventListener('change', (event) => {
+
+  if (inputVerification() === false) {
+    generateButtonElement.setAttribute('disabled', 'true')
+  }
+  else {
+    generateButtonElement.removeAttribute('disabled')
+  }
+})
+
+generateButtonElement.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  if (inputVerification() === false) {
+    generateButtonElement.setAttribute('disabled', 'true')
+  }
+  else {
     generateImage()
   }
 })
@@ -96,17 +144,17 @@ customizeMenuCancelButtonElement.addEventListener('click', () => {
   customizeMenuElement.classList.remove('is-active')
 })
 
-paletteResolutionInputElements.forEach((element, index) => {
-  element.addEventListener("change", (event) => {
-    imageSize[index] = event.target.value
-    canvasSizeUpdate()
-  })
-})
+// paletteResolutionInputElements.forEach((element, index) => {
+//   element.addEventListener("change", (event) => {
+//     imageSize[index] = event.target.value
+//     canvasSizeUpdate()
+//   })
+// })
 
-pixelSizeInputElement.addEventListener('change', (event) => {
-  pixelSize = event.target.value
-  canvasSizeUpdate()
-})
+// pixelSizeInputElement.addEventListener('change', (event) => {
+//   pixelSize = event.target.value
+//   canvasSizeUpdate()
+// })
 
 // colorSelectElement.addEventListener('change', (event) => {
 //   const hex = event.target.value 
